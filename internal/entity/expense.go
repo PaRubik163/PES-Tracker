23efc619d5bafe0a2c90a9cd6f7d66b77c95f5c4
Ruben <rubengevorkan40203@gmail.com>
2025-10-04 21:04:03 +0300

    fix: change metadate in entity expense

--- internal/entity/expense.go
+++ internal/entity/expense.go
@@ -12,7 +12,7 @@ type Expense struct{
 	Decription string   `gorm:"column:description" json:"description"`
 	Category string		`gorm:"notNull;column:category" json:"category"`
 	Amount decimal.Decimal `gorm:"type:numeric(12,2);notNull;column:amount" json:"amount"`
-	Date time.Time	    `gorm:"type:date;notNull;column:expense_date; json:"expense_date"`
+	Date time.Time	    `gorm:"type:date;notNull;column:expense_date" json:"expense_date"`
 	UserID	int		    `gorm:"notNull;column:user_id" json:"user_id`	
 	User 	User        `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE" json:"-"`
 } 

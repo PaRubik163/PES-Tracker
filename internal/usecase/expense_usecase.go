8892d7f388455bc4491df54c8600dac33ebb0dd1
Ruben <rubengevorkan40203@gmail.com>
2025-10-04 21:30:42 +0300

    feat: add expense usecase

--- internal/usecase/expense_usecase.go
+++ internal/usecase/expense_usecase.go
@@ -1,6 +1,9 @@
 package usecase
 
-import "tracker/internal/repository"
+import (
+	"tracker/internal/entity"
+	"tracker/internal/repository"
+)
 
 type ExpenseUseCase struct{
 	expenseRepository *repository.ExpenseRepository
@@ -11,3 +14,29 @@ func NewExpenseUseCase(expenseRepo *repository.ExpenseRepository) *ExpenseUseCas
 		expenseRepository: expenseRepo,
 	}
 } 
+
+func (expUseCase *ExpenseUseCase) AddExpense(expense *entity.Expense) error {
+	if err := expUseCase.expenseRepository.Create(expense); err != nil{
+		return err
+	}
+
+	return nil
+}
+
+func (expUseCase *ExpenseUseCase) GetAllExpenses(userID int) ([]*entity.Expense,error) {
+	expenses, err := expUseCase.expenseRepository.GetAll(userID)
+
+	if err != nil{
+		return nil, err
+	}
+
+	return expenses, nil
+}
+
+func (expUseCase *ExpenseUseCase) DeleteExpense(expenseID, userID int) error {
+	if err := expUseCase.expenseRepository.DeleteByID(expenseID, userID); err != nil{
+		return err
+	}
+
+	return nil
+}
